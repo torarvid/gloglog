7278931feba374142bc2ff34f3fec0e920306399
Tor Arvid Lund <torarvid@gmail.com>
2022-10-11 21:42:13 +0200

    Move some stuff from main to model

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"github.com/pelletier/go-toml/v2"
-	"github.com/torarvid/gloglog/table"
 )
 
 func loadConfig() Config {
@@ -36,47 +33,8 @@ func main() {
 
 	config := loadConfig()
 	search := config.SavedSearches[0]
-	columns := make([]table.ColumnSpec[string], len(search.Columns))
-	for i, c := range search.Columns {
-		columns[i] = ColumnFromConfig(c)
-	}
 
-	filename, exists := search.Options["filename"]
-	if !exists {
-		panic("filename not found")
-	}
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	rows := make([]string, 0)
-	for scanner.Scan() {
-		rows = append(rows, scanner.Text())
-	}
-
-	t := table.New(
-		table.WithColumns(columns),
-		table.WithRows(rows),
-		table.WithFocused[string](true),
-		table.WithHeight[string](27),
-	)
-
-	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("27")).
-		Bold(false)
-	t.SetStyles(s)
-
-	m := model{table: t, zoomRow: false}
+	m := newModel(search)
 	if err := tea.NewProgram(m, tea.WithAltScreen()).Start(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
--- model.go
+++ model.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"log"
+	"os"
 	"strings"
 	"time"
 
@@ -15,9 +18,59 @@ var baseStyle = lipgloss.NewStyle().
 	BorderStyle(lipgloss.RoundedBorder()).
 	BorderForeground(lipgloss.Color("240"))
 
+const (
+	stateTable = iota
+	stateZoomRow
+	stateSearch
+)
+
 type model struct {
-	table   table.Model[string]
-	zoomRow bool
+	table table.Model[string]
+	state int
+}
+
+func newModel(search SavedSearch) *model {
+	columns := make([]table.ColumnSpec[string], len(search.Columns))
+	for i, c := range search.Columns {
+		columns[i] = ColumnFromConfig(c)
+	}
+
+	filename, exists := search.Options["filename"]
+	if !exists {
+		panic("filename not found")
+	}
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	rows := make([]string, 0)
+	for scanner.Scan() {
+		rows = append(rows, scanner.Text())
+	}
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused[string](true),
+		table.WithHeight[string](27),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("27")).
+		Bold(false)
+	t.SetStyles(s)
+
+	return &model{table: t, state: stateTable}
 }
 
 func (m model) Init() tea.Cmd { return nil }
@@ -36,7 +89,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "q", "ctrl+c":
 			return m, tea.Quit
 		case " ":
-			m.zoomRow = !m.zoomRow
+			if m.state == stateZoomRow {
+				m.state = stateTable
+			} else {
+				m.state = stateZoomRow
+			}
 		}
 
 	case tea.WindowSizeMsg:
@@ -48,7 +105,12 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	if m.zoomRow {
+	switch m.state {
+
+	case stateTable:
+		return baseStyle.Render(m.table.View()) + "\n"
+
+	case stateZoomRow:
 		rawRow := m.table.SelectedRow()
 		var row map[string]interface{}
 		json.Unmarshal([]byte(rawRow), &row)
@@ -57,8 +119,10 @@ func (m model) View() string {
 			return "Invalid"
 		}
 		return baseStyle.Render(string(rendered))
+
+	default:
+		panic("Unknown state")
 	}
-	return baseStyle.Render(m.table.View()) + "\n"
 }
 
 type Column struct {
