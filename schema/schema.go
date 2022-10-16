1d47ab931dd673dc80415166864b9537b14d6b05
Tor Arvid Lund <torarvid@gmail.com>
2022-10-17 01:03:38 +0200

    Slight progress on schema editing

--- schema/schema.go
+++ schema/schema.go
@@ -4,47 +4,70 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"strconv"
 
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/torarvid/gloglog/config"
 )
 
 var (
-	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+	titleStyle        = lipgloss.NewStyle().MarginLeft(2).MarginTop(1)
 	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
 	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
 	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
-	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
-	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1).PaddingRight(4)
+	detailStyle       = lipgloss.NewStyle().
+				BorderLeft(true).
+				BorderStyle(lipgloss.NormalBorder()).
+				PaddingLeft(2).
+				PaddingRight(4)
 )
 
 type Attribute struct {
-	Name      string
-	Width     int
-	Selectors []string
-	Type      string
-	Format    *string
+	Name       string
+	nameInput  textinput.Model
+	Width      int
+	widthInput textinput.Model
+	Selectors  []string
+	Type       string
+	Format     *string
 }
 
 func (a Attribute) FilterValue() string { return "" }
 
+func (a Attribute) View() string {
+	log.Println("PrintAttr", a.nameInput.Value(), a.nameInput.View())
+	return "Name\n" + a.nameInput.View() + "\n\n" + "Width\n" + a.widthInput.View()
+}
+
 type Model struct {
 	Attributes []Attribute
 	list       list.Model
-	choice     *Attribute
+	selected   *Attribute
 }
 
 func FromLogView(lv config.LogView, width, height int) Model {
 	attrs := make([]Attribute, len(lv.Attrs))
 	for i, attr := range lv.Attrs {
+		nameInput := textinput.New()
+		nameInput.Placeholder = "Title/Name/Barcode/Host/..."
+		nameInput.SetValue(attr.Name)
+		nameInput.CharLimit = 30
+		nameInput.Focus()
+		widthInput := textinput.New()
+		widthInput.Placeholder = "15"
+		widthInput.SetValue(strconv.Itoa(attr.Width))
 		attrs[i] = Attribute{
-			Name:      attr.Name,
-			Width:     attr.Width,
-			Selectors: []string{attr.Selector},
-			Type:      attr.Type,
-			Format:    attr.Format,
+			Name:       attr.Name,
+			nameInput:  nameInput,
+			Width:      attr.Width,
+			widthInput: widthInput,
+			Selectors:  []string{attr.Selector},
+			Type:       attr.Type,
+			Format:     attr.Format,
 		}
 	}
 	items := make([]list.Item, len(attrs))
@@ -55,7 +78,7 @@ func FromLogView(lv config.LogView, width, height int) Model {
 	log.Println("Create schema")
 	l := list.New(items, itemDelegate{}, width, height)
 	l.Title = "Schema attributes"
-	l.SetShowStatusBar(true)
+	l.SetShowStatusBar(false)
 	l.SetFilteringEnabled(false)
 	l.Styles.Title = titleStyle
 	l.Styles.PaginationStyle = paginationStyle
@@ -68,10 +91,9 @@ func (m Model) Init() tea.Cmd {
 }
 
 func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
-	log.Println("Schema Update")
+	cmds := make([]tea.Cmd, 0)
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		log.Println("Schema: WindowSizeMsg")
 		m.list.SetSize(msg.Width-5, msg.Height-5)
 		return m, nil
 
@@ -80,7 +102,7 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 		case "enter":
 			attr, ok := m.list.SelectedItem().(Attribute)
 			if ok {
-				m.choice = &attr
+				m.selected = &attr
 			}
 			return m, nil
 		}
@@ -88,14 +110,28 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 
 	var cmd tea.Cmd
 	m.list, cmd = m.list.Update(msg)
-	return m, cmd
+	cmds = append(cmds, cmd)
+	if m.selected != nil {
+		log.Println("Selected", m.selected.nameInput.Value())
+		m.selected.nameInput, cmd = m.selected.nameInput.Update(msg)
+		log.Println("Selected2", m.selected.nameInput.Value())
+		cmds = append(cmds, cmd)
+		m.selected.widthInput, cmd = m.selected.widthInput.Update(msg)
+		cmds = append(cmds, cmd)
+	}
+	return m, tea.Batch(cmds...)
 }
 
 func (m Model) View() string {
-	if m.choice != nil {
-		return quitTextStyle.Render(fmt.Sprintf("%s? Sounds good to me.", m.choice.Name))
+	if m.selected != nil {
+		log.Println("View", m.selected.nameInput.Value())
 	}
-	return "\n" + m.list.View()
+	attrList := m.list.View()
+	selectionView := ""
+	if m.selected != nil {
+		selectionView = detailStyle.Height(m.list.Height()).Render(m.selected.View())
+	}
+	return lipgloss.JoinHorizontal(lipgloss.Left, attrList, selectionView)
 }
 
 type itemDelegate struct{}
