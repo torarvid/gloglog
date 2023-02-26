4be9e615e6a37edcdfb31862260ae437e88eda1d
Tor Arvid Lund <torarvid@gmail.com>
2023-02-26 17:16:54 +0100

    Add/remove fields from schema

--- schema/schema.go
+++ schema/schema.go
@@ -50,62 +50,76 @@ func (a Attribute) View() string {
 	return strings.Join(parts, "\n\n")
 }
 
-type KeyMap struct {
+type KeyMapMain struct {
+	EnterDetail key.Binding
+	Exit        key.Binding
+	NewField    key.Binding
+	DeleteField key.Binding
+}
+
+type KeyMapDetail struct {
 	SelectNextField key.Binding
 	SelectPrevField key.Binding
 }
 
 type Model struct {
-	Attributes []Attribute
-	list       list.Model
-	selected   *int
-	keyMap     KeyMap
+	Attributes   []Attribute
+	list         list.Model
+	selected     *int
+	keyMap       KeyMapMain
+	detailKeyMap KeyMapDetail
+}
+
+func createAttribute(attr config.Attribute) Attribute {
+	nameInput := textinput.New()
+	nameInput.Placeholder = "Title/Name/Barcode/Host/..."
+	nameInput.SetValue(attr.Name)
+	nameInput.CharLimit = 30
+	nameInput.Focus()
+
+	widthInput := textinput.New()
+	widthInput.Placeholder = "15"
+	widthInput.SetValue(strconv.Itoa(attr.Width))
+	widthInput.Blur()
+
+	selectorsInput := textinput.New()
+	selectorsInput.Placeholder = "error.code / server.host.ip / ..."
+	selectorsInput.SetValue("[\"" + strings.Join(attr.Selectors, "\", \"") + "\"]")
+	selectorsInput.Blur()
+
+	typeInput := textinput.New()
+	typeInput.Placeholder = "string / int / time / ..."
+	typeInput.SetValue(attr.Type)
+	typeInput.Blur()
+
+	formatInput := textinput.New()
+	formatInput.Placeholder = "2006-01-02 15:04:05.000"
+	if attr.Format != nil {
+		formatInput.SetValue(*attr.Format)
+	}
+	formatInput.Blur()
+
+	return Attribute{
+		Name:      attr.Name,
+		Width:     attr.Width,
+		Selectors: attr.Selectors,
+		Type:      attr.Type,
+		Format:    attr.Format,
+		inputs:    []textinput.Model{nameInput, widthInput, selectorsInput, typeInput, formatInput},
+	}
 }
 
 func FromLogView(lv config.LogView, width, height int) Model {
 	attrs := make([]Attribute, len(lv.Attrs))
 	for i, attr := range lv.Attrs {
-		nameInput := textinput.New()
-		nameInput.Placeholder = "Title/Name/Barcode/Host/..."
-		nameInput.SetValue(attr.Name)
-		nameInput.CharLimit = 30
-		nameInput.Focus()
-
-		widthInput := textinput.New()
-		widthInput.Placeholder = "15"
-		widthInput.SetValue(strconv.Itoa(attr.Width))
-		widthInput.Blur()
-
-		selectorsInput := textinput.New()
-		selectorsInput.Placeholder = "error.code / server.host.ip / ..."
-		selectorsInput.SetValue("[\"" + strings.Join(attr.Selectors, "\", \"") + "\"]")
-		selectorsInput.Blur()
-
-		typeInput := textinput.New()
-		typeInput.Placeholder = "string / int / time / ..."
-		typeInput.SetValue(attr.Type)
-		typeInput.Blur()
-
-		formatInput := textinput.New()
-		formatInput.Placeholder = "2006-01-02 15:04:05.000"
-		if attr.Format != nil {
-			formatInput.SetValue(*attr.Format)
-		}
-		formatInput.Blur()
-
-		attrs[i] = Attribute{
-			Name:      attr.Name,
-			Width:     attr.Width,
-			Selectors: attr.Selectors,
-			Type:      attr.Type,
-			Format:    attr.Format,
-			inputs:    []textinput.Model{nameInput, widthInput, selectorsInput, typeInput, formatInput},
-		}
+		attrs[i] = createAttribute(attr)
 	}
 	items := listItemsFromAttributes(attrs)
 
 	log.Println("Create schema")
-	l := list.New(items, itemDelegate{}, width, height)
+	keyMap := MainKeyMap()
+	keys := []key.Binding{keyMap.EnterDetail, keyMap.Exit, keyMap.NewField, keyMap.DeleteField}
+	l := list.New(items, itemDelegate{keys}, width, height)
 	l.Title = "Schema attributes"
 	l.SetShowStatusBar(false)
 	l.SetFilteringEnabled(false)
@@ -117,7 +131,7 @@ func FromLogView(lv config.LogView, width, height int) Model {
 	l.Styles.PaginationStyle = paginationStyle
 	l.Styles.HelpStyle = helpStyle
 	l.DisableQuitKeybindings()
-	return Model{Attributes: attrs, list: l, keyMap: DefaultKeyMap()}
+	return Model{Attributes: attrs, list: l, keyMap: keyMap, detailKeyMap: DetailKeyMap()}
 
 }
 
@@ -130,8 +144,28 @@ func listItemsFromAttributes(attrs []Attribute) []list.Item {
 	return items
 }
 
-func DefaultKeyMap() KeyMap {
-	return KeyMap{
+func MainKeyMap() KeyMapMain {
+	return KeyMapMain{
+		EnterDetail: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter detail", "Enter"),
+		),
+		Exit: key.NewBinding(
+			key.WithKeys("esc"),
+			key.WithHelp("exit", "Esc"),
+		),
+		NewField: key.NewBinding(
+			key.WithKeys("n"),
+			key.WithHelp("new field", "n"),
+		),
+		DeleteField: key.NewBinding(
+			key.WithKeys("d"),
+			key.WithHelp("delete field", "d"),
+		),
+	}
+}
+func DetailKeyMap() KeyMapDetail {
+	return KeyMapDetail{
 		SelectNextField: key.NewBinding(
 			key.WithKeys("tab"),
 			key.WithHelp("next field", "Tab"),
@@ -157,42 +191,66 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 		return m, nil
 
 	case tea.KeyMsg:
-		switch keypress := msg.String(); keypress {
-		case "enter":
+		switch {
+		case key.Matches(msg, m.keyMap.EnterDetail):
 			if m.selected == nil {
 				m.selectAttr(m.list.Index())
 				return m, nil
-			} else {
-				i := *m.selected
-				m.Attributes[i].Name = m.Attributes[i].inputs[0].Value()
-				if w, err := strconv.Atoi(m.Attributes[i].inputs[1].Value()); err == nil {
-					m.Attributes[i].Width = w
-				}
-				var selectors []string
-				if err := json.Unmarshal([]byte(m.Attributes[i].inputs[2].Value()), &selectors); err == nil {
-					m.Attributes[i].Selectors = selectors
-				}
-				m.Attributes[i].Type = m.Attributes[i].inputs[3].Value()
-				if m.Attributes[i].inputs[4].Value() != "" {
-					format := m.Attributes[i].inputs[4].Value()
-					m.Attributes[i].Format = &format
-				} else {
-					m.Attributes[i].Format = nil
-				}
-				m.deselect()
-				m.list.SetItems(listItemsFromAttributes(m.Attributes))
-				return m, m.UpdateSchema()
 			}
-		case "esc":
+			i := *m.selected
+			m.Attributes[i].Name = m.Attributes[i].inputs[0].Value()
+			if w, err := strconv.Atoi(m.Attributes[i].inputs[1].Value()); err == nil {
+				m.Attributes[i].Width = w
+			}
+			var selectors []string
+			if err := json.Unmarshal([]byte(m.Attributes[i].inputs[2].Value()), &selectors); err == nil {
+				m.Attributes[i].Selectors = selectors
+			}
+			m.Attributes[i].Type = m.Attributes[i].inputs[3].Value()
+			if m.Attributes[i].inputs[4].Value() != "" {
+				format := m.Attributes[i].inputs[4].Value()
+				m.Attributes[i].Format = &format
+			} else {
+				m.Attributes[i].Format = nil
+			}
+			m.deselect()
+			m.list.SetItems(listItemsFromAttributes(m.Attributes))
+			return m, m.UpdateSchema()
+		case key.Matches(msg, m.keyMap.Exit):
 			if m.selected == nil {
 				return m, func() tea.Msg { return Close{} }
 			}
 			m.deselect()
-		}
-		switch {
-		case key.Matches(msg, m.keyMap.SelectNextField):
+		case key.Matches(msg, m.keyMap.NewField):
+			if m.selected != nil {
+				break
+			}
+			attr := createAttribute(config.Attribute{})
+			m.Attributes = append(m.Attributes, attr)
+			listItems := m.list.Items()
+			listItems = append(listItems, &attr)
+			m.list.SetItems(listItems)
+			m.selectAttr(len(m.Attributes) - 1)
+			return m, nil
+		case key.Matches(msg, m.keyMap.DeleteField):
+			if m.selected != nil {
+				break
+			}
+			if len(m.Attributes) == 0 {
+				return m, nil
+			}
+			m.Attributes = append(
+				m.Attributes[:m.list.Index()], m.Attributes[m.list.Index()+1:]...,
+			)
+			listItems := m.list.Items()
+			listItems = append(
+				listItems[:m.list.Index()], listItems[m.list.Index()+1:]...,
+			)
+			m.list.SetItems(listItems)
+			return m, m.UpdateSchema()
+		case key.Matches(msg, m.detailKeyMap.SelectNextField):
 			m.focusNextInput()
-		case key.Matches(msg, m.keyMap.SelectPrevField):
+		case key.Matches(msg, m.detailKeyMap.SelectPrevField):
 			m.focusPrevInput()
 		}
 	}
@@ -269,19 +327,19 @@ func (m *Model) selectAttr(i int) {
 	m.selected = &i
 	m.list.KeyMap.CursorDown.SetEnabled(false)
 	m.list.KeyMap.CursorUp.SetEnabled(false)
-	m.keyMap.SelectNextField.SetEnabled(true)
-	m.keyMap.SelectPrevField.SetEnabled(true)
+	m.detailKeyMap.SelectNextField.SetEnabled(true)
+	m.detailKeyMap.SelectPrevField.SetEnabled(true)
 }
 
 func (m *Model) deselect() {
 	m.selected = nil
 	m.list.KeyMap.CursorDown.SetEnabled(true)
 	m.list.KeyMap.CursorUp.SetEnabled(true)
-	m.keyMap.SelectNextField.SetEnabled(false)
-	m.keyMap.SelectPrevField.SetEnabled(false)
+	m.detailKeyMap.SelectNextField.SetEnabled(false)
+	m.detailKeyMap.SelectPrevField.SetEnabled(false)
 }
 
-type itemDelegate struct{}
+type itemDelegate struct{ keys []key.Binding }
 
 func (d itemDelegate) Height() int                               { return 1 }
 func (d itemDelegate) Spacing() int                              { return 0 }
@@ -302,4 +360,8 @@ func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list
 	}
 
 	fmt.Fprint(w, fn(str))
+}
+func (d itemDelegate) ShortHelp() []key.Binding { return d.keys }
+func (d itemDelegate) FullHelp() [][]key.Binding {
+	return [][]key.Binding{d.ShortHelp()}
 }
